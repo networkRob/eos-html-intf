3f7c337efed112c66a50a4abca50c3624d06a494
Rob Martin <robmartin@arista.com>
2019-12-02 08:50:59 -0600

    updated rmpbuild

--- buildrpm.sh
+++ buildrpm.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-while getopts v:r:i option
+while getopts v:r:i:t option
 do
 case "${option}"
 in
 v) VERSION=${OPTARG};;
 r) RELEASE=${OPTARG};;
 i) DIMAGE=${OPTART};;
+t) DTAG=${OPTART};;
 esac
 done
 mkdir -p rpmbuild/SOURCES
 mkdir -p rpmbuild/RPM
 tar -cvf rpmbuild/SOURCES/EosIntfGui-${VERSION}-${RELEASE}.tar source/*
 
-docker run --rm -v $(pwd)/rpmbuild/SPECS/:/home/builder/rpmbuild/SPECS:ro -v $(pwd)/rpmbuild/SOURCES:/home/builder/rpmbuild/SOURCES:ro -v $(pwd)/rpmbuild/RPM:/home/builder/rpmbuild/RPM:rw ${DIMAGE}
+docker run --rm -v $(pwd)/rpmbuild/SPECS/:/home/builder/rpmbuild/SPECS:ro -v $(pwd)/rpmbuild/SOURCES:/home/builder/rpmbuild/SOURCES:ro -v $(pwd)/rpmbuild/RPM:/home/builder/rpmbuild/RPM:rw ${DIMAGE}:${DTAG}
