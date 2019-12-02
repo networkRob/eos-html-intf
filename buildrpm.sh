3058a9065319a409ce603ced08de14adb92306ac
Rob Martin <robmartin@arista.com>
2019-12-02 08:53:40 -0600

    fixed opt arg

--- buildrpm.sh
+++ buildrpm.sh
@@ -6,12 +6,11 @@ case "${option}"
 in
 v) VERSION=${OPTARG};;
 r) RELEASE=${OPTARG};;
-i) DIMAGE=${OPTART};;
-t) DTAG=${OPTART};;
+i) DIMAGE=${OPTARG};;
 esac
 done
 mkdir -p rpmbuild/SOURCES
 mkdir -p rpmbuild/RPM
 tar -cvf rpmbuild/SOURCES/EosIntfGui-${VERSION}-${RELEASE}.tar source/*
 
-docker run --rm -v $(pwd)/rpmbuild/SPECS/:/home/builder/rpmbuild/SPECS:ro -v $(pwd)/rpmbuild/SOURCES:/home/builder/rpmbuild/SOURCES:ro -v $(pwd)/rpmbuild/RPM:/home/builder/rpmbuild/RPM:rw ${DIMAGE}:${DTAG}
+docker run --rm -v $(pwd)/rpmbuild/SPECS/:/home/builder/rpmbuild/SPECS:ro -v $(pwd)/rpmbuild/SOURCES:/home/builder/rpmbuild/SOURCES:ro -v $(pwd)/rpmbuild/RPM:/home/builder/rpmbuild/RPM:rw ${DIMAGE}
