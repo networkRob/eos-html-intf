cd4778f764d5f400bec05577758cd5bf509479db
Rob Martin <robmartin@arista.com>
2020-08-25 19:57:52 +0000

    streamlined swix creation

--- buildrpm.sh
+++ buildrpm.sh
@@ -8,6 +8,8 @@ v) VERSION=${OPTARG};;
 r) RELEASE=${OPTARG};;
 esac
 done
+
+DUT='192.168.23.5'
 mkdir -p rpmbuild/SOURCES
 mkdir -p rpmbuild/RPM
 tar -cvf rpmbuild/SOURCES/EosIntfGui-${VERSION}-${RELEASE}.tar source/*
@@ -25,6 +27,9 @@ echo "primaryRPM: EosIntfGui-${VERSION}-${RELEASE}.noarch.rpm" >> manifest.txt
 echo -n "EosIntfGui-${VERSION}-${RELEASE}.noarch.rpm: " >> manifest.txt
 echo $(sha1sum rpmbuild/RPM/noarch/EosIntfGui-${VERSION}-${RELEASE}.noarch.rpm | awk '{print $1}') >> manifest.txt
 
-scp /workspaces/eos-html-intf/rpmbuild/RPM/noarch/EosIntfGui-${VERSION}-${RELEASE}.noarch.rpm rmartin@192.168.23.5:/mnt/flash/ext-eos/
-scp manifest.txt rmartin@192.168.23.5:/mnt/flash/ext-eos/
+scp -i ~/.ssh/builder /workspaces/eos-html-intf/rpmbuild/RPM/noarch/EosIntfGui-${VERSION}-${RELEASE}.noarch.rpm builder@${DUT}:/mnt/flash/ext-eos/
+scp -i ~/.ssh/builder manifest.txt builder@${DUT}:/mnt/flash/ext-eos/
 
+ssh -i ~/.ssh/builder builder@${DUT} bash swix create /mnt/flash/ext-eos/swix/EosIntfGui-${VERSION}-${RELEASE}.swix /mnt/flash/ext-eos/EosIntfGui-${VERSION}-${RELEASE}.noarch.rpm
+
+scp -i ~/.ssh/builder builder@${DUT}:/mnt/flash/ext-eos/swix/EosIntfGui-${VERSION}-${RELEASE}.swix /workspaces/eos-html-intf/extension/
